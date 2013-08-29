07eec3e85e4c24bd4fa03d349425225349e511d1
Brian Lange <thebrianlange@gmail.com>
2013-08-29 17:31:18 -0500

    Add init and setup.py for PyPI/installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dix',
+      version='0.1',
+      description='A utility for quantifying large plaintext data using a '\
+                 +'revolutionary metric: Moby-Dicks',
+      url='https://github.com/datascopeanalytics/dix',
+      author='Datascope Analytics',
+      author_email='dix.heads@datascopeanalytics.com',
+      license='WTFPL',
+      scripts=['bin/dix'],
+      zip_safe=False)
