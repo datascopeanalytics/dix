ae2afa20cd1ca7a2474c1d9056617d551e10f02b
Brian Lange <thebrianlange@gmail.com>
2013-08-30 12:09:22 -0500

    Bump version number for new release to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dix',
-      version='0.1',
+      version='0.11',
       description='A utility for quantifying large plaintext data using a '\
                  +'revolutionary metric: Moby-Dicks',
       url='https://github.com/datascopeanalytics/dix',
